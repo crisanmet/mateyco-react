95fd6dd0f635a3a00d836c2143e8b8c677641395
Cristian Sancricca <cristiansancricca@gmail.com>
2021-12-01 15:47:21 -0300

    Se agregan algunos estilos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <h1>MateYCo</h1>
-      <a href="#home">Home</a>
-      <a href="#productos">Productos</a>
-      <a href="#nosotros">Nosotros</a>
-      <a href="#contacto">Contacto</a>
-    </nav>
+    <header>
+      <nav className="navbar">
+        <h1>MateYCo</h1>
+        <a href="#home">Home</a>
+        <a href="#productos">Productos</a>
+        <a href="#nosotros">Nosotros</a>
+        <a href="#contacto">Contacto</a>
+      </nav>
+    </header>
   );
 }
 

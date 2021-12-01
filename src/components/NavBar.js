ac36478d1609b8693930a8c3ace116ddbc1d692c
Cristian Sancricca <cristiansancricca@gmail.com>
2021-12-01 13:22:37 -0300

    componente navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>MateYCo</h1>
+      <a href="#home">Home</a>
+      <a href="#productos">Productos</a>
+      <a href="#nosotros">Nosotros</a>
+      <a href="#contacto">Contacto</a>
+    </nav>
+  );
+}
+
+export default NavBar;

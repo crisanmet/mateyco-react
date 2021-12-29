0bb11e6e670f5c2b7448d92b76b9fd55a1e95f6c
Cristian Sancricca <cristiansancricca@gmail.com>
2021-12-29 14:51:08 -0300

    Cart count

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,13 +2,21 @@ import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
+import "../CartWidget/CartWidget.css";
 
 function CartWidget() {
   const [cart, setCart] = useContext(CartContext);
   return (
     <div>
-      <FontAwesomeIcon icon={faShoppingCart} size="2x"></FontAwesomeIcon>
-      <span>{cart.length} </span>
+      <Link to="/cart">
+        <FontAwesomeIcon
+          icon={faShoppingCart}
+          size="2x"
+          className="icon"
+        ></FontAwesomeIcon>
+        <span className="count">{cart.length} </span>
+      </Link>
     </div>
   );
 }

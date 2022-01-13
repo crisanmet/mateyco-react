34f4a485d9df0d88659c73bbbc87d762b2de5107
Cristian Sancricca <cristiansancricca@gmail.com>
2022-01-13 17:19:32 -0300

    checkout inicio

---
+++ src/components/CheckOut/CheckOut.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import "../CheckOut/CheckOut.css";
+
+export const CheckOut = () => {
+  const [cart, setCart] = useContext(CartContext);
+
+  return <></>;
+};
